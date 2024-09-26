3434cdd2a6b500ebec2607bfaebd8925c3563dd9
TrueBag <72657453+TrueBag@users.noreply.github.com>
2024-09-26 10:46:58 +0300

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates
+# Author: True Bag
+# Date: 26 September 2024
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:
+# Any other information needed?
 
 
 #### Workspace setup ####
